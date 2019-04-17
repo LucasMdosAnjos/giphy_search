a13126c582539a06d7358b4b32a56935b0674b26
LucasMdosAnjos <lucasmon@live.com>
2019-04-17 19:43:02 -0300

    version 1

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import './UI/home_page.dart';
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
